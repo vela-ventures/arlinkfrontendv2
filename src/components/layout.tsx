756a2c0dbad0e9501d62313fcb73aa1ab2cce57c
internet_trash <naiknischal92@gmail.com>
2024-10-19 17:46:39 +0530

    ui revamp checkpoint: fix layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -15,14 +15,14 @@ export default function Layout({ children }: { children?: React.ReactNode }) {
     return (
         <div className={cn(
             "flex flex-col bg-gray-100 dark:bg-neutral-800 w-full flex-1 mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
-            "min-h-screen"
+            "min-h-screen bg-background"
         )}>
             <Navbar />
-            <div className="flex flex-1">
+          
                 <div className="p-2 md:p-10  border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full max-h-screen overflow-scroll">
                     {connected ? children : "Connect Wallet to continue :)"}
                 </div>
             </div>
-        </div>
+      
     );
 }
