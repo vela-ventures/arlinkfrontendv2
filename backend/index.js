120378edd160e67abb88e43ff227d31d52f0e2cd
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-10-22 18:17:26 +0530

    remove limits

--- backend/index.js
+++ backend/index.js
@@ -547,5 +547,4 @@ const server = app.listen(PORT, () => {
 
 initRegistry().catch(console.error);
 
-server.setTimeout(60 * 60 * 10 * 1000);
-server.keepAliveTimeout = 60 * 60 * 10 * 1000;
+server();
