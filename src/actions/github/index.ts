cb60840996dd145f140f41b0f39bf3b870b3dd99
internet_trash <naiknischal92@gmail.com>
2025-02-05 03:36:05 +0530

    add org scope

--- src/actions/github/index.ts
+++ src/actions/github/index.ts
@@ -13,7 +13,7 @@ export async function initiateGitHubAuth() {
             : "https://arlink.arweave.net";
 
     const redirectUri = `${BASE_URL}/deploy`;
-    const authUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${redirectUri}&scope=repo`;
+    const authUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${redirectUri}&scope=repo read:org`;
     window.location.href = authUrl;
 }
 
