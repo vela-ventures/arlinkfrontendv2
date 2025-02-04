cf3368e9275276d195ffcfcff13f7016e0eb57c2
DMZTdhruv <shruvparmar@gmail.com>
2025-02-04 19:13:17 +0000

    Fixed template card height issue

--- src/components/template-selector.tsx
+++ src/components/template-selector.tsx
@@ -303,94 +303,82 @@ export default function TemplateSelector({
         </div>
     );
 }
-
 const TemplateCard = ({ template }: { template: TemplateDashboard }) => {
     return (
         <div
             key={template.ID}
-            className="block h-full relative rounded-md overflow-hidden border bg-arlink-bg-secondary-color border-neutral-800 hover:border-neutral-700 transition-colors"
+            className="block h-fit rounded-lg overflow-hidden border bg-arlink-bg-secondary-color border-neutral-800 hover:border-neutral-700 transition-colors"
         >
-            <div className="flex flex-col h-full">
+            <div className="aspect-video pt-1 px-1 relative">
+                <img
+                    src={template.ThumbnailUrl || "/placeholder-clone.jpg"}
+                    alt={template.Name}
+                    className="object-cover object-center w-full h-full border rounded-md border-neutral-900"
+                    onError={(e) => {
+                        e.currentTarget.src = "/placeholder-clone.jpg";
+                    }}
+                />
+                <span className="bg-neutral-900 border absolute bottom-3 left-3 border-neutral-800 w-8 h-8 rounded-full flex items-center justify-center text-sm">
+                    {
+                        frameworks.find(
+                            (f) =>
+                                f.id ===
+                                template.Framework?.toLowerCase()
+                                    .split(".")
+                                    .join(""),
+                        )?.icon
+                    }
+                </span>
+            </div>
+
+            <div className="p-2 flex flex-col gap-2">
                 <Link
                     to={`/templates/${template.Framework}/${template.Name}/${template.ID}`}
-                    className="relative w-full"
                 >
-                    <div className="aspect-[5/3] relative w-full">
-                        <img
-                            src={
-                                template.ThumbnailUrl ||
-                                "/placeholder-clone.jpg"
-                            }
-                            alt={template.Name}
-                            className="absolute inset-0 object-cover object-center w-full h-full border rounded-sm border-neutral-900"
-                            onError={(e) => {
-                                e.currentTarget.src = "/placeholder-clone.jpg";
-                            }}
-                        />
-                        <span className="bg-neutral-900 border absolute bottom-3 left-3 border-neutral-800 w-8 h-8 rounded-full flex items-center justify-center text-sm">
-                            {
-                                frameworks.find(
-                                    (f) =>
-                                        f.id ===
-                                        template.Framework?.toLowerCase()
-                                            .split(".")
-                                            .join(""),
-                                )?.icon
-                            }
-                        </span>
+                    <div className="px-1">
+                        <div className="flex items-center space-x-2">
+                            <span className="font-semibold">
+                                {template.Name}
+                            </span>
+                        </div>
+                        <p className="text-sm text-neutral-400 line-clamp-2">
+                            {template.Description}
+                        </p>
                     </div>
                 </Link>
 
-                <div className="flex flex-col flex-1 p-2">
+                <div className="p-2 flex gap-2 items-center justify-between">
                     <Link
-                        to={`/templates/${template.Framework}/${template.Name}/${template.ID}`}
-                        className="flex-1"
+                        to={`https://github.com/${extractOwnerName(
+                            template.RepoUrl || "",
+                        )}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="flex items-center gap-1.5 text-sm text-neutral-500"
                     >
-                        <div className="px-1">
-                            <div className="flex items-center space-x-2">
-                                <span className="font-semibold">
-                                    {template.Name}
-                                </span>
-                            </div>
-                            <p className="text-sm text-neutral-400 line-clamp-2">
-                                {template.Description}
-                            </p>
-                        </div>
+                        <svg
+                            width="16"
+                            height="16"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="-translate-y-0.2"
+                        >
+                            <path
+                                d="M12 2C10.6868 2 9.38642 2.25866 8.17317 2.7612C6.95991 3.26375 5.85752 4.00035 4.92893 4.92893C3.05357 6.8043 2 9.34784 2 12C2 16.42 4.87 20.17 8.84 21.5C9.34 21.58 9.5 21.27 9.5 21V19.31C6.73 19.91 6.14 17.97 6.14 17.97C5.68 16.81 5.03 16.5 5.03 16.5C4.12 15.88 5.1 15.9 5.1 15.9C6.1 15.97 6.63 16.93 6.63 16.93C7.5 18.45 8.97 18 9.54 17.76C9.63 17.11 9.89 16.67 10.17 16.42C7.95 16.17 5.62 15.31 5.62 11.5C5.62 10.39 6 9.5 6.65 8.79C6.55 8.54 6.2 7.5 6.75 6.15C6.75 6.15 7.59 5.88 9.5 7.17C10.29 6.95 11.15 6.84 12 6.84C12.85 6.84 13.71 6.95 14.5 7.17C16.41 5.88 17.25 6.15 17.25 6.15C17.8 7.5 17.45 8.54 17.35 8.79C18 9.5 18.38 10.39 18.38 11.5C18.38 15.32 16.04 16.16 13.81 16.41C14.17 16.72 14.5 17.33 14.5 18.26V21C14.5 21.27 14.66 21.59 15.17 21.5C19.14 20.16 22 16.42 22 12C22 10.6868 21.7413 9.38642 21.2388 8.17317C20.7362 6.95991 19.9997 5.85752 19.0711 4.92893C18.1425 4.00035 17.0401 3.26375 15.8268 2.7612C14.6136 2.25866 13.3132 2 12 2Z"
+                                fill="gray"
+                            />
+                        </svg>
+                        {template.CreatorName}
                     </Link>
-
-                    <div className="mt-4 flex gap-2 items-center justify-between">
-                        <Link
-                            to={`https://github.com/${extractOwnerName(
-                                template.RepoUrl || "",
-                            )}`}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="flex items-center gap-1.5 text-sm text-neutral-500"
-                        >
-                            <svg
-                                width="16"
-                                height="16"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="-translate-y-0.2"
-                            >
-                                <path
-                                    d="M12 2C10.6868 2 9.38642 2.25866 8.17317 2.7612C6.95991 3.26375 5.85752 4.00035 4.92893 4.92893C3.05357 6.8043 2 9.34784 2 12C2 16.42 4.87 20.17 8.84 21.5C9.34 21.58 9.5 21.27 9.5 21V19.31C6.73 19.91 6.14 17.97 6.14 17.97C5.68 16.81 5.03 16.5 5.03 16.5C4.12 15.88 5.1 15.9 5.1 15.9C6.1 15.97 6.63 16.93 6.63 16.93C7.5 18.45 8.97 18 9.54 17.76C9.63 17.11 9.89 16.67 10.17 16.42C7.95 16.17 5.62 15.31 5.62 11.5C5.62 10.39 6 9.5 6.65 8.79C6.55 8.54 6.2 7.5 6.75 6.15C6.75 6.15 7.59 5.88 9.5 7.17C10.29 6.95 11.15 6.84 12 6.84C12.85 6.84 13.71 6.95 14.5 7.17C16.41 5.88 17.25 6.15 17.25 6.15C17.8 7.5 17.45 8.54 17.35 8.79C18 9.5 18.38 10.39 18.38 11.5C18.38 15.32 16.04 16.16 13.81 16.41C14.17 16.72 14.5 17.33 14.5 18.26V21C14.5 21.27 14.66 21.59 15.17 21.5C19.14 20.16 22 16.42 22 12C22 10.6868 21.7413 9.38642 21.2388 8.17317C20.7362 6.95991 19.9997 5.85752 19.0711 4.92893C18.1425 4.00035 17.0401 3.26375 15.8268 2.7612C14.6136 2.25866 13.3132 2 12 2Z"
-                                    fill="gray"
-                                />
-                            </svg>
-                            {template.CreatorName}
-                        </Link>
-                        <Link
-                            to={template.DemoUrl}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="flex items-center hover:underline text-sm gap-2 font-semibold rounded-sm"
-                        >
-                            <ExternalLink size={16} />
-                        </Link>
-                    </div>
+                    <Link
+                        to={template.DemoUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="flex items-center hover:underline text-sm gap-2 font-semibold rounded-sm"
+                    >
+                        <ExternalLink size={16} />
+                    </Link>
                 </div>
             </div>
         </div>
