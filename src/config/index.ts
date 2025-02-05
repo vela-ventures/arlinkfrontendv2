ead610db011b4faedcd83f6e3b3e8d0007a07f59
internet_trash <naiknischal92@gmail.com>
2025-02-05 15:52:09 +0530

    update vars '

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ const isTestEnv = import.meta.env.VITE_ENV === "test";
 
 export const GITHUB_CLIENT_ID = isTestEnv
     ? import.meta.env.VITE_GITHUB_CLIENT_ID
-    : "Iv23linjZLxHZeHfSgqB";
+    : "Ov23liNTUkzDZLMjSE1T";
 
 export const GITHUB_CLIENT_ID_TEMPLATE = isTestEnv
     ? import.meta.env.VITE_GITHUB_CLIENT_ID_TEMPLATE
